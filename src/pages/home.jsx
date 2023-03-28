2649eb1f4ebb28f866008bc80b5fdae8afcabc45
Arham Alam <arham.alam.21cse@bmu.edu.in>
2023-03-28 15:37:24 +0530

    API is added

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div>HomePage</div>;
+  const [seacrhStr, setSeacrhStr] = useState("");
+
+  const onSeacrhInputChange = (ev) => {
+    setSeacrhStr(ev.target.value);
+  };
+
+  const onSeacrh = async (ev) => {
+    ev.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${seacrhStr}`
+    );
+    const body = await response.json();
+
+  };
+
+  // https://api.tvmaze.com/search/shows?q=girls
+
+  return (
+    <div>
+      <form onSubmit={onSeacrh}>
+        <input type="text" value={seacrhStr} onChange={onSeacrhInputChange} />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
